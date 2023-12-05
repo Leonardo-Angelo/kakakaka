ea72542ad5a5ed29a3365c98a3ad1f153e2810ea
REGISTRO - JAYME ALMEIDA PAIVA DOUTOR <e035178w10@professor.educacao.sp.gov.br>
2023-12-05 11:05:39 -0300

    df

---
+++ jacasvrpt.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="my-custom-style">
+      <h1>Meu Aplicativo React</h1>
+      <p>Bem-vindo ao meu aplicativo React!</p>
+    </div>
+  );
+}
+
+export default App;
